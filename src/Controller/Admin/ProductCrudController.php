842b2999e677b060b2e80040dab8b8e1bc53bcd5
pierre defauquet <pierredefauquet@gmail.com>
2024-06-28 11:35:04 +0200

    maj#6 création crud product

---
+++ src/Controller/Admin/ProductCrudController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class ProductCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Product::class;
+    }
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            IdField::new('id')->hideOnForm(),
+            TextField::new('nom'),
+            textField::new('titre'),
+            TextareaField::new('description'),
+            NumberField::new('prix'),
+            ImageField::new('image')
+                ->setBasePath('images/')
+                ->setUploadDir('public/images/productImg/')
+                ->setRequired($pageName !== Crud::PAGE_EDIT)
+                ->setFormTypeOptions($pageName !== Crud::PAGE_EDIT ? ['allow_delete' => false] : []),
+
+            AssociationField::new('marque'),
+        ];
+    }
+}
